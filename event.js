49b52e537c78386c3518f6cbfeab97731f9072fd
Ingrid <lkoh93@gmail.com>
2026-03-30 20:17:54 +0900

    add events files

---
+++ event.js
@@ -0,0 +1,19 @@
+{
+  id: "event_001",
+  category: "스터디벙",
+  subCategory: "열공",
+  region: "의왕",
+  title: "같이 공부할 사람!",
+  date: "2026-03-30",
+  day: "월",
+  startTime: "14:30",
+  endTime: "20:30",
+  location: "포일 청년발전소",
+  createdBy: "Ingrid",
+  attendees: [
+    { nickname: "Ingrid", joinedAt: "2026-03-30T10:00:00" }
+  ],
+  comments: [
+    { nickname: "Lisa", text: "나도 갈게!", createdAt: "2026-03-30T10:05:00"}
+  ]
+}
