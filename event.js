a65c2ff70b4d2399e73c5f8f7c0b47a03cb4155d
Ingrid <lkoh93@gmail.com>
2026-03-31 15:28:43 +0900

    add nicknamew system

--- event.js
+++ event.js
@@ -1,19 +1,8 @@
-{
-  id: "event_001",
-  category: "스터디벙",
-  subCategory: "열공",
-  region: "의왕",
-  title: "같이 공부할 사람!",
-  date: "2026-03-30",
-  day: "월",
-  startTime: "14:30",
-  endTime: "20:30",
-  location: "포일 청년발전소",
-  createdBy: "Ingrid",
-  attendees: [
-    { nickname: "Ingrid", joinedAt: "2026-03-30T10:00:00" }
-  ],
-  comments: [
-    { nickname: "Lisa", text: "나도 갈게!", createdAt: "2026-03-30T10:05:00"}
-  ]
+let nickname = localStorage.getItem("nickname");
+
+if (!nickname) {
+  nickname = prompt("환영합니다! 닉네임을 입력해주세요 🤠");
+  localStorage.setItem("nickname", nickname);
 }
+
+console.log("현재 사용자: ${nickname}");
